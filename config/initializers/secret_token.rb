dfe77c2fad34b374c1882fcae5f961157ef51173
vinay <vinay@vertisinfotech.com>
2013-02-12 13:04:19 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rohtak::Application.config.secret_token = 'f4cdaea11c8942cd128f6e3cdea613c8cd761dedf834616407eb6ec9f78dca538eb98e2bfdb7cc93566161cac94feac04cb0eae60e3541caf28d33acff7ea1e4'
